2d3200abe30ae07cce516d488e53c7596e8f9ba0
Tom Callaway <spot@fedoraproject.org>
2013-09-08 23:19:05 -0400

    fixup color display too

--- nxt_rviz_plugin/src/nxt_ultrasonic_display.h
+++ nxt_rviz_plugin/src/nxt_ultrasonic_display.h
@@ -75,7 +75,6 @@ public:
   // Overrides from Display
   virtual void targetFrameChanged() {}
   virtual void fixedFrameChanged();
-  virtual void createProperties();
   virtual void update(float wall_dt, float ros_dt);
   virtual void reset();
 
